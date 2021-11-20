01d7c41f2f2553c3d1bd835e71bfbd79a9ae5e29
Michael Aïvázis <michael.aivazis@para-sim.com>
2021-11-20 08:10:36 -0800

    ux/views: moved the top level page styling to {views}

--- ux/client/styles.js
+++ ux/client/styles.js
@@ -10,19 +10,6 @@ import { theme } from '~/palette'
 
 // styling for the page container
 export default {
-    // the top level flex container
-    page : {
-        // placement
-        width: "100%",
-        height: "100%",
-
-        // overall styling
-        backgroundColor: theme.page.background,
-
-        // for my children
-        display: "flex",
-        flexDirection: "column",
-    },
 }
 
 
